d74dff74e299d5a3006c53e331fb7eeb818812ac
Bernadette McKnight <sissyhanks@yahoo.com>
2021-09-10 09:34:24 -0400

    reconfigure modles

--- models/DailyWorkout.js
+++ models/DailyWorkout.js
@@ -1,11 +1,45 @@
 const mongoose = require('mongoose');
-const exercise = { discriminatorKey: 'kind' };
 
 const DailyWorkoutSchema = new mongoose.Schema({
   day: {
     type: Date,
     default: Date.now
   },
-}, exercise);
+  exercises: [{
+    type: {
+    type: String,
+    required: true,
+    trim: true
+    },
+
+    name: {
+      type: String,
+      required: true,
+      trim: true
+    },
+
+    duration: {
+      type: Number,
+      required: true,
+      trim: true      
+    },
+
+    weight: {
+      type: Number,  
+    },
+
+    reps: {
+      type: Number,
+    },
+
+    sets: {
+      type: Number,
+    },
+
+    distance: {
+      type: Number,
+    }
+  }]
+});
 
 module.exports = mongoose.model('DailyWorkout', DailyWorkoutSchema);
--- models/index.js
+++ models/index.js
@@ -1,6 +1,5 @@
 //export an object containing all models
 
 module.exports = {
-  Cardio: require('./Cardio'),
-  Resistance: require('./Resistance')
+  DailyWorkout: require('./DailyWorkout')
 };
