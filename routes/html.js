6c60815ec95d2a7b594fc8a05ff97bf84efac769
Bernadette McKnight <sissyhanks@yahoo.com>
2021-09-10 11:37:29 -0400

    get stats route

--- routes/html.js
+++ routes/html.js
@@ -2,12 +2,15 @@ const express = require('express');
 const router = express.Router();
 const path = require("path");
 
-// const Story = require ('../models/Story');
 
-//add stories / Show add page
-//Get /stories/add
+//this route will open the exercise html page 
+//Get /exercise
 router.get('/exercise', (req, res) => {
   res.sendFile(path.join(__dirname, "../public/exercise.html"));
+});
+
+router.get('/stats', (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/stats.html"))
 });
 
 
