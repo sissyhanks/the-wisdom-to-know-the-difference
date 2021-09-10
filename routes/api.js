2e9b972b49c09cfff8d9b5ad378246e2c10ea5bd
Bernadette McKnight <sissyhanks@yahoo.com>
2021-09-10 13:31:27 -0400

    api get routes

--- routes/api.js
+++ routes/api.js
@@ -2,4 +2,50 @@ const express = require('express');
 const router = express.Router();
 const path = require("path");
 
+const db = require('../models')
+
+//process add stories
+//post api/workouts
+
+
+router.get("/workouts", (req, res) => {
+  db.Workout.find({})
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+router.get("/workouts/range", (req, res) => {
+  db.Workout.find({})
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+router.post("/workouts", ({body}, res) => {
+  db.Workout.create(body)
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+router.put("/workouts/:id", ({body}, res) => {
+  db.Workout.create(body)
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
 module.exports = router;
