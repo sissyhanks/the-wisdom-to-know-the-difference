9eeede514f1f6206e4a1bbd648a43d993b1968cf
Bernadette McKnight <sissyhanks@yahoo.com>
2021-09-10 10:22:01 -0400

    conect to DB

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const dotenv = require('dotenv');
-// const connectDB = require('./config/db');
+const connectDB = require('./config/db');
 dotenv.config({ path: './config/config.env'});
 
-// connectDB();
+connectDB();
 const app = express();
 
 app.use(express.urlencoded({ extend: true }));
@@ -16,17 +16,11 @@ const PORT = process.env.PORT || 3031;
 
 const db = require("./models");
 
-
-
 app.use(logger("dev"));
-
-
 
 app.use(express.static("public"));
 
 app.use('/', require('./routes'));
-
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitnessjourney", { useNewUrlParser: true });
 
 app.listen(PORT, () => {
   console.log(`your fitness journey begins on ${PORT}`);
